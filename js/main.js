353dc39525a2468d3fdf8a89c77c4a7dee74fdd4
grayd22 <pitenko.artem@gmail.com>
2020-11-12 19:41:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,12 +3,7 @@ let menuToggle = document.querySelector('#menu-toggle');
 // Создаем переменную, в которую положим меню
 let menu = document.querySelector('.sidebar');
 // отслеживаем клик по кнопке меню и запускаем функцию 
-menuToggle.addEventListener('click', function (event) {
-  // отменяем стандартное поведение ссылки
-  event.preventDefault();
-  // вешаем класс на меню, когда кликнули по кнопке меню 
-  menu.classList.toggle('visible');
-})
+
 
 const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
 
@@ -29,6 +24,8 @@ const editUsername = document.querySelector('.edit-username');
 const editPhotoURL = document.querySelector('.edit-photo');
 
 const userAvatarElem = document.querySelector ('.user-avatar');
+
+const postsWrapper = document.querySelector('.posts');
 
 const listUsers = [
   {
@@ -105,6 +102,38 @@ const setUsers = {
   } 
 };
 
+const setPosts = {
+  allPosts: [
+    {
+      title: 'Заголовок поста',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags:['свежее','новое','горячее','мое','случайность'],
+      author: 'maks@mail.com',
+      date: '11.11.2020, 20:54:00',
+      like: 15,
+      comments: 20,
+    },
+    {
+      title: 'Заголовок поста2',
+      text: 'Первый 13-дюймовый MacBook Pro с чипом Apple Silicon заменяет младшие модели с процессорами Intel и двумя портами Thunderbolt. Новый чип Apple M1 может похвастаться более мощным 8-ядерным процессором и 8-ядерным графическим процессором с 16-ядерным Neural Engine, благодаря чему новый MacBook Pro в 3 раза превышает производительность самого продаваемого ноутбука под управлением Windows.',
+      tags:['свежее','новое','мое','случайность'],
+      author: 'kate@mail.com',
+      date: '10.11.2020, 20:54:00',
+      like: 45,
+      comments: 15,
+    },
+    {
+      title: 'Заголовок поста3',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags:['свежее','новое','мое','случайность'],
+      author: 'kate@mail.com',
+      date: '09.11.2020, 20:54:00',
+      like: 25,
+      comments: 5,
+    }
+  ]
+};
+
 const toggleAuthDom = () => {
   const user = setUsers.user;
   console.log('user: ',user);
@@ -120,7 +149,67 @@ const toggleAuthDom = () => {
   }
 };
 
-//логин
+const showAllPosts = () => {
+  let postsHTML = ''; 
+
+  setPosts.allPosts.forEach(({title, text, date}) => {
+    
+    postsHTML +=`
+    <section class="post">
+        <div class="post-body">
+          <h2 class="post-title">${title}</h2>
+          <p class="post-text">${text}</p>
+          <div class="tags">
+            <a href="#" class="tag">#свежее</a>
+            
+          </div>
+        
+        </div>
+        
+        <div class="post-footer">
+          <div class="post-buttons">
+            <button class="post-button likes">
+              <svg width="19" height="20" class="icon icon-like">
+                <use xlink:href="img/icons.svg#like"></use>
+              </svg>
+              <span class="likes-counter">26</span>
+            </button>
+            <button class="post-button comments">
+              <svg width="21" height="21" class="icon icon-comment">
+                <use xlink:href="img/icons.svg#comment"></use>
+              </svg>
+              <span class="comments-counter">157</span>
+            </button>
+            <button class="post-button save">
+              <svg width="19" height="19" class="icon icon-save">
+                <use xlink:href="img/icons.svg#save"></use>
+              </svg>
+            </button>
+            <button class="post-button share">
+              <svg width="17" height="19" class="icon icon-share">
+                <use xlink:href="img/icons.svg#share"></use>
+              </svg>
+            </button>
+          </div>
+          
+          <div class="post-author">
+            <div class="author-about">
+              <a href="#" class="author-username">arteislamov</a>
+              <span class="post-time">${date}</span>
+            </div>
+            <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+          </div>
+        </div>
+        </section>
+        `;
+  })
+
+
+  postsWrapper.innerHTML = postsHTML;
+}
+
+const init = () => {
+  //логин
 loginForm.addEventListener('submit', event => {
   event.preventDefault();
   
@@ -163,5 +252,18 @@ editContainer.addEventListener('submit', event=> {
   editContainer.classList.remove('visible'); 
 });
 
+menuToggle.addEventListener('click', function (event) {
+  // отменяем стандартное поведение ссылки
+  event.preventDefault();
+  // вешаем класс на меню, когда кликнули по кнопке меню 
+  menu.classList.toggle('visible');
+})
 
-toggleAuthDom();
+  showAllPosts();
+  toggleAuthDom();
+}
+
+document.addEventListener('DOMContentLoaded', init)
+
+
+
